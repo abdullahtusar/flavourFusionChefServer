ba33c4084865ce5e28c5a8f3b12fa9ac70ad4a5b
abdullahtusar <abdullahtusar2@gmail.com>
2023-05-03 02:25:17 +0600

    added chef and recipe data into the index.js file

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
+const chef = require('./data/chef.json');
+const recipes = require('./data/recipes.json')
 
 app.use(cors())
 
@@ -9,6 +11,7 @@ app.get('/', (req, res) =>{
     res.send('Recipe site is running')
 });
 
+app.get
 
 app.listen(port, ()=>{
     console.log(`recipe API is running on port: ${port}`)
