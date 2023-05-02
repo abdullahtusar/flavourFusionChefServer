5fb8174ffe66e248761d7612cf1c9f542b7a8b40
abdullahtusar <abdullahtusar2@gmail.com>
2023-05-03 02:30:26 +0600

    two json data file successfully show in localhost

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 const chef = require('./data/chef.json');
-const recipes = require('./data/recipes.json')
+const recipes = require('./data/recipes.json');
 
 app.use(cors())
 
@@ -11,7 +11,13 @@ app.get('/', (req, res) =>{
     res.send('Recipe site is running')
 });
 
-app.get
+app.get('/chef', (req, res)=>{
+    res.send(chef)
+});
+
+app.get('/recipes', (req, res)=>{
+    res.send(recipes)
+});
 
 app.listen(port, ()=>{
     console.log(`recipe API is running on port: ${port}`)
