b65b9e4627132f1a761aabf11bc5b332ecc2be7b
abdullahtusar <abdullahtusar2@gmail.com>
2023-05-03 09:49:01 +0600

    chef and recipes id wise link added

--- index.js
+++ index.js
@@ -19,6 +19,20 @@ app.get('/recipes', (req, res)=>{
     res.send(recipes)
 });
 
+app.get('/chef/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const selectedChef = chef.find(c => c.id == id);
+    res.send(selectedChef);
+});
+
+app.get('/recipes/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const chefRecipes = recipes.filter(r => r.recipe_id == id);
+    res.send(chefRecipes);
+})
+
 app.listen(port, ()=>{
     console.log(`recipe API is running on port: ${port}`)
 })
